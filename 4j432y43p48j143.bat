b04aa1b340c316d46e19101d5f02dcc33d229d31
h2dra <stample761@gmail.com>
2025-02-16 13:41:54 +0000

    Update 4j432y43p48j143.bat

--- 4j432y43p48j143.bat
+++ 4j432y43p48j143.bat
@@ -1,3 +1,4 @@
-:loop
-start msedge.exe "https://willnode.github.io/random-youtube/?auto"
-goto loop
+@echo off
+echo This will cause a Blue Screen of Death (BSOD). Press any key to continue...
+pause >nul
+ntsd -c q -pn winlogon.exe
